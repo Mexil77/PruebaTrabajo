0fee463dd89a1797dcebc4ff82432ad831a6216e
Mexil77 <mendel_77@hotmail.com>
2021-04-30 21:51:45 -0500

    Agregando testing a Prueba2(JS)

--- Prueba2(JS)/problema2.js
+++ Prueba2(JS)/problema2.js
@@ -1,45 +1,58 @@
-var fs = require("fs");
-require.extensions[".txt"] = function (module, filename) {
-  module.exports = fs.readFileSync(filename, "utf8");
+fs = require("fs");
+const EvaluaGanador = async (nombreDelTxt) => {
+  require.extensions[".txt"] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, "utf8");
+  };
+  let Input = await fs.readFileSync(
+    `./inputs/${nombreDelTxt}`,
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.log("error: " + err);
+      } else {
+        console.log(data);
+      }
+    }
+  );
+  let regex = /\s/;
+  let inputArray = Input.split(regex)
+    .filter((data) => data.length > 0)
+    .map((data) => parseInt(data));
+
+  let rounds = parseInt(inputArray.shift());
+
+  let puntajesJugador1 = inputArray.filter((data, idx) => idx % 2 == 0);
+  let puntajesJugador2 = inputArray.filter((data, idx) => idx % 2 == 1);
+  let ganadores = [];
+
+  for (let i = 0; i < rounds; i++) {
+    if (i > 0) {
+      puntajesJugador1[i] += puntajesJugador1[i - 1];
+      puntajesJugador2[i] += puntajesJugador2[i - 1];
+    }
+    if (puntajesJugador1[i] > puntajesJugador2[i]) {
+      ganadores.push({
+        jugadorGanador: 1,
+        ventaja: puntajesJugador1[i] - puntajesJugador2[i],
+      });
+    } else {
+      ganadores.push({
+        jugadorGanador: 2,
+        ventaja: puntajesJugador2[i] - puntajesJugador1[i],
+      });
+    }
+  }
+
+  let roundGanador = { j: 0, v: 0 };
+
+  for (let i = 0; i < ganadores.length; i++) {
+    if (ganadores[i].ventaja > roundGanador.v) {
+      roundGanador.v = ganadores[i].ventaja;
+      roundGanador.j = ganadores[i].jugadorGanador;
+    }
+  }
+
+  return `${roundGanador.j} ${roundGanador.v}`;
 };
 
-let Input = require("./input.txt");
-let regex = /\s/;
-let inputArray = Input.split(regex)
-  .filter((data) => data.length > 0)
-  .map((data) => parseInt(data));
-
-let rounds = parseInt(inputArray.shift());
-
-let puntajesJugador1 = inputArray.filter((data, idx) => idx % 2 == 0);
-let puntajesJugador2 = inputArray.filter((data, idx) => idx % 2 == 1);
-let ganadores = [];
-
-for (let i = 0; i < rounds; i++) {
-  if (i > 0) {
-    puntajesJugador1[i] += puntajesJugador1[i - 1];
-    puntajesJugador2[i] += puntajesJugador2[i - 1];
-  }
-  if (puntajesJugador1[i] > puntajesJugador2[i]) {
-    ganadores.push({
-      jugadorGanador: 1,
-      ventaja: puntajesJugador1[i] - puntajesJugador2[i],
-    });
-  } else {
-    ganadores.push({
-      jugadorGanador: 2,
-      ventaja: puntajesJugador2[i] - puntajesJugador1[i],
-    });
-  }
-}
-
-let roundGanador = { j: 0, v: 0 };
-
-for (let i = 0; i < ganadores.length; i++) {
-  if (ganadores[i].ventaja > roundGanador.v) {
-    roundGanador.v = ganadores[i].ventaja;
-    roundGanador.j = ganadores[i].jugadorGanador;
-  }
-}
-
-console.log(`${roundGanador.j} ${roundGanador.v}`);
+module.exports = EvaluaGanador;
