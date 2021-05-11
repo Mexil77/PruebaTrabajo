a6d0a664fd5371ee189f50385487ac2c828a0bbb
Mexil77 <mendel_77@hotmail.com>
2021-05-10 22:28:38 -0500

    Agregando validaciones a Prueba1

--- Prueba1(JS)/problema1.js
+++ Prueba1(JS)/problema1.js
@@ -14,26 +14,64 @@ const encuentraPalabra = async (rutaDelTxt) => {
   let regex = /\s/;
   let inputArray = Input.split(regex).filter((data) => data.length > 0);
 
-  let M1 = inputArray[3]
+  let M1 = inputArray[0];
+  let M2 = inputArray[1];
+  let N = inputArray[2];
+
+  if (M1 <= 2 || M1 > 50) {
+    return "Primera instruccion de dimencion no permitida";
+  }
+  if (M2 <= 2 || M2 > 50) {
+    return "Segunda instruccion de dimencion no permitida";
+  }
+
+  if (N <= 3 || N > 5000) {
+    return "Mensaje de dimencion no permitida";
+  }
+
+  if (/[^a-zA-Z0-9]/.test(N)) {
+    return "Mensaje con caracteres no validos";
+  }
+
+  let primerInstruccionLetrasArray = inputArray[3];
+  for (let i = 0; i < primerInstruccionLetrasArray.length; i++) {
+    if (
+      i < primerInstruccionLetrasArray.length - 1 &&
+      primerInstruccionLetrasArray[i] === primerInstruccionLetrasArray[i + 1]
+    ) {
+      return "Primer instruccion tiene letras repetidas";
+    }
+  }
+  let segundaInstruccionLetrasArray = inputArray[3];
+  for (let i = 0; i < segundaInstruccionLetrasArray.length; i++) {
+    if (
+      i < segundaInstruccionLetrasArray.length - 1 &&
+      segundaInstruccionLetrasArray[i] === segundaInstruccionLetrasArray[i + 1]
+    ) {
+      return "Segunda instruccion tiene letras repetidas";
+    }
+  }
+
+  let primerInstruccion = inputArray[3]
     .split("")
     .map((data) => data + "+")
     .join("");
-  let M2 = inputArray[4]
+  let segundaInstruccion = inputArray[4]
     .split("")
     .map((data) => data + "+")
     .join("");
-  let N = inputArray[5];
+  let mensaje = inputArray[5];
 
-  let regex1 = new RegExp(`${M1}`);
-  let regex2 = new RegExp(`${M2}`);
+  let regex1 = new RegExp(`${primerInstruccion}`);
+  let regex2 = new RegExp(`${segundaInstruccion}`);
 
-  let find1 = regex1.test(N);
-  let find2 = regex2.test(N);
+  let find1 = regex1.test(mensaje);
+  let find2 = regex2.test(mensaje);
 
   let res = "";
 
   if (find1) {
-    res += "SI";
+    return "SI\nNO";
   } else {
     res += "NO";
   }
